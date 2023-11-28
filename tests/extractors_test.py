1a2d892503e765add25ddf0c6757306bae58a767
Ben Davies <133011765+ben-leadtech@users.noreply.github.com>
2023-11-28 15:54:32 +0000

    Changes to be committed: 	modified:   tests/extractors_test.py

--- tests/extractors_test.py
+++ tests/extractors_test.py
@@ -1,13 +1,46 @@
 import pytest
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 import pandas as pd
-from ..src.extractors import SalesforceExtractor, BigQueryExtractor
+from typing import Any
+#from ..src.extractors import SalesforceExtractor, BigQueryExtractor
 from ..src.extractors import ExtractorJob, MultiExtractor
 from ..src.containers import Data
 
+
+
+class MockQueryResult:
+	def to_dataframe(self):
+		return pd.DataFrame()
+
+class MockClient:
+	"""
+	Mock class for Salesforce, SalesforceBulk and BigQuery.
+	"""
+	def __init__(self, **kwargs):
+		pass
+
+	def query_all(self, query: str):
+		return pd.DataFrame()
+
+	def create_query_job(self, query: str):
+		return None
+
+	def query(self, job: Any, query: str):
+		return MockQueryResult()
+
+	def is_batch_done(self, job: Any):
+		return True
+
+	def get_all_results_for_query_batch(self, job: Any):
+		return {}
+
+
+
 #================================================================================#
 def test_ExtractorJob():
-	ej = ExtractorJob(query='query',name='name',extractor=SalesforceExtractor())
+	with patch('simple_salesforce.Salesforce', new=MockClient):
+		from ..src.extractors import SalesforceExtractor
+		ej = ExtractorJob(query='query',name='name',extractor=SalesforceExtractor())
 	assert hasattr(ej,'query')
 	assert hasattr(ej,'name')
 	assert hasattr(ej,'extractor')
@@ -17,20 +50,27 @@ def test_ExtractorJob():
 
 #================================================================================#
 def test_extractors():
-	extractors = [SalesforceExtractor(), BigQueryExtractor()]
+	with patch('simple_salesforce.Salesforce', new=MockClient):
+		with patch('google.cloud.bigquery.Client', new=MockClient):
+			from ..src.extractors import SalesforceExtractor, BigQueryExtractor
+			extractors = [SalesforceExtractor(), BigQueryExtractor()]
 	for ex in extractors:
 		assert hasattr(ex,'client')
 		assert hasattr(ex,'query_runner')
 
 #================================================================================#
 def test_salesforce_extractors():
-	sf = SalesforceExtractor(bulk=True)
+	with patch('simple_salesforce.Salesforce', new=MockClient):
+		from ..src.extractors import SalesforceExtractor
+		sf = SalesforceExtractor(bulk=True)
 	assert hasattr(sf,'client')
 	assert hasattr(sf,'query_runner')
 	assert sf.client.__class__.__name__ == 'SalesforceBulk'
 	assert sf.query_runner.__name__ == '_bulk_query'
 
-	sf = SalesforceExtractor(bulk=False)
+	with patch('salesforce_bulk.SalesforceBulk', new=MockClient):
+		from ..src.extractors import SalesforceExtractor
+		sf = SalesforceExtractor(bulk=False)
 	assert hasattr(sf,'client')
 	assert hasattr(sf,'query_runner')
 	assert sf.client.__class__.__name__ == 'Salesforce'
@@ -40,7 +80,9 @@ def test_salesforce_extractors():
 
 #================================================================================#
 def test_BigQueryExtractor():
-	bq = BigQueryExtractor()
+	with patch('google.cloud.bigquery.Client', new=MockClient):
+		from ..src.extractors import BigQueryExtractor
+		bq = BigQueryExtractor()
 	assert hasattr(bq,'client')
 	assert hasattr(bq,'query_runner')
 	assert bq.client.__class__.__name__ == 'Client'
@@ -66,8 +108,10 @@ class TestMultiExtractor:
 	#______________________________________________________________________________#
 	# Test create_job
 	def test_mex_create_job(self):
-		me = MultiExtractor()
-		me.create_job(query='query',name='name',extractor=SalesforceExtractor())
+		with patch('simple_salesforce.Salesforce', new=MockClient):
+			from ..src.extractors import SalesforceExtractor
+			me = MultiExtractor()
+			me.create_job(query='query',name='name',extractor=SalesforceExtractor())
 		assert me.extractor_jobs[0].query == 'query'
 		assert me.extractor_jobs[0].name == 'name'
 		assert me.extractor_jobs[0].extractor.__class__.__name__ == 'SalesforceExtractor'
@@ -77,6 +121,7 @@ class TestMultiExtractor:
 	#______________________________________________________________________________#
 	# Test run
 	def test_mex_run(self):
+		from ..src.extractors import SalesforceExtractor
 		mock_sf = Mock(SalesforceExtractor)
 		mock_sf.query_runner = Mock(return_value=pd.DataFrame())
 		me = MultiExtractor()
